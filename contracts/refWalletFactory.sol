8ebb83abeabe219c490b29e1de2f5bf78d23fa5b
panaromafinance <124047482+panaromafinance@users.noreply.github.com>
2023-05-03 12:58:26 +0530

    pair0 fix

--- contracts/refWalletFactory.sol
+++ contracts/refWalletFactory.sol
@@ -17,6 +17,16 @@ contract refWalletFactory {
 
     constructor() public {
         owner = msg.sender;
+        address _pair;
+        bytes memory bytecode = type(refWallet).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(msg.sender, address(0)));
+        assembly {
+          _pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+
+        IrefWallet(_pair).initialize(msg.sender, address(0));
+        getUser[msg.sender].LockPair = _pair;
+        getUser[msg.sender].Referal = address(0);
     }
 
     modifier onlyOwner {
@@ -25,6 +35,7 @@ contract refWalletFactory {
     }
 
     function createRefAddress(address _parent) external returns(address _pair){
+        require(getUser[_parent].LockPair != address(0), "Invalid Referal Address!");
         require(msg.sender != _parent, "Cyclic Referal Restricted!");
         bytes memory bytecode = type(refWallet).creationCode;
         require(getUser[msg.sender].LockPair == address(0), "Referal Pair Exist!");
